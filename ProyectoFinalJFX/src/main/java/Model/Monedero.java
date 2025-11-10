2e8dbfc8f0919308e23ed1322139f3d454ee4d9f
Melany Gomez <melagoba91@gmail.com>
2025-11-10 13:25:48 -0500

    clase monedero
    creacion de los atributo, constructor, getters y setters de la clase

--- ProyectoFinalJFX/src/main/java/Model/Monedero.java
+++ ProyectoFinalJFX/src/main/java/Model/Monedero.java
@@ -1,4 +1,38 @@
 package Model;
+import java.util.ArrayList;
 
 public class Monedero {
+    protected String idMonedero;
+    protected double saldo;
+    protected ArrayList<Transaccion> historialTransacciones;
+
+    public Monedero(String idMonedero, double saldo) {
+        this.idMonedero = idMonedero;
+        this.saldo = saldo;
+        this.historialTransacciones = new ArrayList<>();
+    }
+
+    public String getIdMonedero() {
+        return idMonedero;
+    }
+
+    public void setIdMonedero(String idMonedero) {
+        this.idMonedero = idMonedero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public ArrayList<Transaccion> getHistorialTransacciones() {
+        return historialTransacciones;
+    }
+
+    public void setHistorialTransacciones(ArrayList<Transaccion> historialTransacciones) {
+        this.historialTransacciones = historialTransacciones;
+    }
 }
